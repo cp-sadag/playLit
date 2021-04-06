c0dd312e653032e562c94a2c4e465ab536635194
Sadagopan P Chakravarthy <cp-sadag@users.noreply.github.com>
2021-04-07 01:31:48 +0530

    reset automl
    reset automl to load scikit breast cancer data to prevent run failures

--- autoML.py
+++ autoML.py
@@ -6,8 +6,8 @@ import pandas as pd
 # Load and split example data
 def setData(inputData, y):
     #data = load_breast_cancer_sk()
-    X, y =  ww.DataTable(inputData), ww.DataColumn(pd.Series(y))
-    #X, y = evalml.demos.load_breast_cancer()
+    #X, y =  ww.DataTable(inputData), ww.DataColumn(pd.Series(y))
+    X, y = evalml.demos.load_breast_cancer()
     X_train, X_test, y_train, y_test = evalml.preprocessing.split_data(X, y, problem_type='binary', test_size = .2)
     return X_train, X_test, y_train, y_test
 
